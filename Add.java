eb3c4f7a29d9c94b229b24475db2e28e296edfa1
vaishnavisusheel <vaishnaviambarkhed@gmail.com>
2025-04-23 14:38:56 +0530

    save

---
+++ Add.java
@@ -0,0 +1,12 @@
+package JAVA;
+
+public class Add {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+    }
+}
